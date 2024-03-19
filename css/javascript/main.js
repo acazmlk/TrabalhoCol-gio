165c71c03da4e04675fe04cc090465b747e00359
Acaz Malko <acazfernandesmalko@hotmail.com>
2024-03-18 21:19:29 -0300

    first commit

---
+++ css/javascript/main.js
@@ -0,0 +1,15 @@
+function TrocarPaginaBebidas () {
+    location.href = "bebidas.html"
+}
+
+function TrocarPaginaCarnes () {
+    location.href = "index.html"
+}
+
+function TrocarPaginaSobremesas () {
+    location.href = "sobremesas.html"
+}
+
+function TrocarPaginaAcompanhamentos () {
+    location.href = "acompanhamentos.html"
+}
